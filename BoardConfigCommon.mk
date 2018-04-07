8110b5b2d6566e2e3bbbd1506f7e26720fc965e7
Yakup Düzgün <yakup.ydzn71@gmail.com>
2018-04-07 17:26:49 +0300

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,7 +200,6 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include vendor/aosp/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Vendor init
